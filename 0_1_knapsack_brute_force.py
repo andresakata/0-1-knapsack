4a9d4d7978a939004d8ddafbe67b236ecdd6f2b4
Andre Sakata <andregsakata@gmail.com>
2018-11-11 19:53:18 -0200

    Add brute force algorithm for 0-1 knapsack problem

---
+++ 0_1_knapsack_brute_force.py
@@ -0,0 +1,39 @@
+class KnapsackBruteForce:
+  def __init__(self, n, weight, profit, max_weight):
+    self.n = n
+    self.weight = weight
+    self.profit = profit
+    self.max_weight = max_weight
+
+  def run(self):
+    self.best_value = 0
+    self.total_weight = 0
+    self.s = []
+    self.solve(self.n - 1, self.s, 0, 0)
+
+  def solve(self, n, s, current_weight, current_value):
+    if n == -1 and current_weight <= self.max_weight and current_value > self.best_value:
+      self.best_value = current_value
+      self.total_weight = current_weight
+      self.s = s.copy()
+
+    if n == -1:
+      return
+
+    self.solve(n - 1, [0] + s, current_weight, current_value)
+    self.solve(n - 1, [1] + s, current_weight + weight[n], current_value + profit[n])
+
+def list_reader(file_name):
+  file = open(file_name).read().split('\n')
+  file.remove('')
+  return list(map(int, file))
+
+weight = list_reader('datasets/p08_w.txt')
+profit = list_reader('datasets/p08_p.txt')
+max_weight = int(open('datasets/p08_c.txt').read())
+
+kbf = KnapsackBruteForce(len(weight), weight, profit, max_weight)
+kbf.run()
+print(kbf.best_value)
+print(kbf.s)
+print(kbf.total_weight)
